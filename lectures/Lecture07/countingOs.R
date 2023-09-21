68e8350779522790cce16f22d04a74f9de6def72
Eliezer Gurarie <egurarie@esf.edu>
2023-09-21 13:47:34 -0400

    lecture 7 & 8 upload

--- lectures/Lecture07/countingOs.R
+++
@@ -1,9 +0,0 @@
-a <- read.csv("clipboard")
-lastnames <- a[,1]
-
-
-tolower(lastnames)
-
-counts <- lapply(letters, grepl, tolower(lastnames))
-names(counts) <- letters
-sapply(counts, sum) %>% sort
